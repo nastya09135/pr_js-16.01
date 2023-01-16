2d32c3c2f2552c8f124ce47504cda735c6898979
nastya09135 <102585161+nastya09135@users.noreply.github.com>
2023-01-16 10:57:12 +0500

    Add files via upload

--- js1.js
+++ js1.js
@@ -2,13 +2,13 @@
 
 //Привяжите всем инпутам следующее событие - по потери фокуса каждый 
 //инпут выводит свое value в абзац с id="test"
-const getInput = document.querySelectorAll('.task');
-const getP = document.querySelector('#test');
+const getInput = document.getElementsByTagName('input');
+const getP = document.getElementById('test');
 
-for (let i = 0; i < getInput.length; i += 1) {
+for (let i = 0; i < getInput.length; i++) {
     getInput[i].addEventListener('blur', func);
 }
 
 function func() {
-    getP.innerHTML = getP.innerHTML + this.value;
+    getP.innerHTML =this.value;
 }
